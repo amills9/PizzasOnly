7aad46d3f992e1c7ec2f7c6c71bedcf4847f6e3e
amills9 <89893713+amills9@users.noreply.github.com>
2021-10-11 12:59:23 +1100

    Update

--- linked1.js
+++ linked1.js
@@ -1,17 +1,36 @@
+/** 
+ * PizzasOnly
+ * Based on the 5 athletes scripts 
+ * @author Andrew Mills 
+ * Created for TAFE NSW Australia
+ * Cert IV Programming
+ * CL - Scripting
+ * @license MIT This project is licensed under the MIT License
+ * @copyright Andrew Mills 2021
+*/
+
+/**
+ * 
+ * Gets number of pizzas from user input and passes into funcation to calculate
+ * 
+ * @param {number} pizzaInput Number passed from HTML file as an int
+ * @returns {number} totalCost The cost of the pizzas after math functions have been performed
+ */
+
 function checkQuantity() {
     let pizzaInput = document.getElementById('quantity').value;
     pizzaInput = Number(pizzaInput); //Convert string to number data type
 
     // declare the pizza pirces for the calculations
     
-    var pizzaPrice = 6.45;
-    var pizzaPrice2 = 12.00;
-    var pizzaPrice3 = 14.00;
+    var pizzaPrice = 6.45;      // 1 Pizza
+    var pizzaPrice2 = 12.00;    // 2 Pizzas
+    var pizzaPrice3 = 14.00;    // 3 Pizzas
 
-    var pizzaCost = 0;
+    var pizzaCost = 0; // Reset cost
 
     // Perform Validation check
-    
+   
     if (!pizzaInput || pizzaInput < 1) {
         if (!pizzaInput) {
             document.getElementById("message").innerHTML = "Please enter a valid pizza quantity";
@@ -38,7 +57,7 @@ function checkQuantity() {
         default:
             let total = pizzaInput;
 
-    // Perform maths if inputer >4
+    // Perform maths if input > 4
 
             do {
                 let pizzaInt = (total / 3);
